def89ef15401fa8c1e42e9b26275268ab7b7b760
Nurin <nurinfazil@hotmail.com>
2020-06-25 13:16:48 -0400

    Test for Netlify deployment

--- src/components/header.js
+++ src/components/header.js
@@ -21,7 +21,7 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `yellow`,
             textDecoration: `none`,
           }}
         >
